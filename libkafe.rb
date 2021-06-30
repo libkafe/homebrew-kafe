b777568d67e89ba6a4510bec37641d5d442d2ce3
Matīss Treinis <matiss@hub256.com>
2021-06-30 21:53:40 +0200

    Update to support Apple SoC

--- libkafe.rb
+++ libkafe.rb
@@ -1,9 +1,9 @@
 class Libkafe < Formula
   desc 'Kafe is an open-source scriptable systems automation toolkit'
   homepage 'https://github.com/libkafe/kafe'
-  url 'https://github.com/libkafe/kafe/archive/v1.1.5.tar.gz'
-  sha256 '8870309dba6e0f1a474ee19e6acf2a51c73f5cd350b16122c637c00f4d40aba7'
-  version '1.1.5'
+  url 'https://github.com/libkafe/kafe/archive/v1.1.5.1.tar.gz'
+  sha256 'dbb9f38d3078cf874f3341e3b8b52ff6292f7a0da8cf7c3b490f8c08cb34c4a3'
+  version '1.1.5.1'
   license 'Apache-2.0'
 
   depends_on 'cmake' => :build
@@ -17,8 +17,27 @@ class Libkafe < Formula
   def install
     system 'mkdir', '-p', 'build/osx'
     Dir.chdir('build/osx')
-    ENV['CC'] = '/usr/local/opt/llvm/bin/clang'
-    ENV['CXX'] = '/usr/local/opt/llvm/bin/clang++'
+
+    if ENV.has_key?('KAFE_CC') then
+        ENV['CC'] = ENV['KAFE_CC']
+    else
+        if File.exist?('/opt/homebrew/opt/llvm/bin/clang') then
+            ENV['CC'] = '/opt/homebrew/opt/llvm/bin/clang'
+        else
+            ENV['CC'] = '/usr/local/opt/llvm/bin/clang'
+        end
+    end
+
+    if ENV.has_key?('KAFE_CXX') then
+        ENV['CXX'] = ENV['KAFE_CXX']
+    else
+        if File.exist?('/opt/homebrew/opt/llvm/bin/clang') then
+            ENV['CXX'] = '/opt/homebrew/opt/llvm/bin/clang++'
+        else
+            ENV['CXX'] = '/usr/local/opt/llvm/bin/clang++'
+        end
+    end
+
     system 'cmake', '../..', *std_cmake_args
     system 'make'
     system 'make', 'install'
